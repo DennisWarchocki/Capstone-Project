3a7a137433dd87e5798bafb6dc9f8efaf108e267
DennisWarchocki <dw.warchocki@gmail.com>
2022-10-06 13:50:26 +0200

    add formular for user to give data to the countdown

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -7,7 +7,14 @@ function Timer() {
 	const [timerHours, setTimerHours] = useState();
 	const [timerMinutes, setTimerMinutes] = useState();
 	const [timerSeconds, setTimerSeconds] = useState();
+	const [userInputDays, setUserInputDays] = useState();
+	const [userInputHours, setUserInputHours] = useState();
+	const [userInputMinutes, setUserInputMinutes] = useState();
+	const [userInputSeconds, setUserInputSeconds] = useState();
 
+	//Ich möchte dass der User z.B. eine Stundenanzahl eintragen kann, und dann der Countdown von dem Wert (z.B. 8h30min) runterzählt evtl statt 'October,8...' einfach Date.now() + (UserInput) = deadline ? und danach bei getTime deadline - date.now
+
+	//Fazit: ------>  -V- Date.now() + (UserTimeInput) = deadline
 	const deadline = 'October, 8, 2022';
 
 	function getTime() {
@@ -35,6 +42,45 @@ function Timer() {
 
 	return (
 		<>
+			<form
+				onSubmit={event => {
+					event.preventDefault();
+					// setUserInput...wie spreche ich mehrere Inputs an?
+					setUserInputDays('');
+				}}
+			>
+				<input
+					placeholder="type Days here"
+					userInputDays={userInputDays}
+					onChange={event => {
+						//(event.target.value) oder (event.target.UserInputDays?)
+						setUserInputDays(event.target.value);
+					}}
+				/>
+				<input
+					placeholder="type hours here"
+					//value={value} -V-
+					userInputHours={userInputHours}
+					onChange={event => {
+						setUserInputHours(event.target.userInputHours);
+					}}
+				/>
+				<input
+					placeholder="type Minutes here"
+					userInputMinutes={userInputMinutes}
+					onChange={event => {
+						setUserInputMinutes(event.target.userInputMinutes);
+					}}
+				/>
+				<input
+					placeholder="type Seconds here"
+					userInputSeconds={userInputSeconds}
+					onChange={event => {
+						setUserInputSeconds(event.target.userInputSeconds);
+					}}
+				/>
+			</form>
+
 			<CookingTimer
 				timerDays={timerDays}
 				timerHours={timerHours}
@@ -44,4 +90,5 @@ function Timer() {
 		</>
 	);
 }
+
 export default Timer;

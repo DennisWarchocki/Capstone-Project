708a671990d3bb716c3f2570a47b2824813ab896
DennisWarchocki <dw.warchocki@gmail.com>
2022-10-07 14:09:20 +0200

    styled Nav Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,13 @@
-import Link from 'next/link';
-
 import {StyledNavButton} from './styled/StyledNavButton';
 
 export default function Header() {
 	return (
 		<header>
 			<nav>
-				<StyledNavButton as="a" href="../pages/index">
-					Styled Timer
+				<StyledNavButton as="a" href="/timer">
+					Cooking Timer
 				</StyledNavButton>
-				<Link href="">Home</Link>
-				<Link href="/timer">Keto Timer</Link>
 			</nav>
 		</header>
 	);
 }
-
-//<Button as="a" href="#">Link with Button styles</Button>
